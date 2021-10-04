ac80860cefeee23fec1c8227ee8236cd94687aee
Izumi Kawashima <schedul.xor@gmail.com>
2021-10-04 15:13:59 +0900

    Don't ignore walk routes

--- xml/scripts/drive.pl
+++ xml/scripts/drive.pl
@@ -30,18 +30,6 @@ while(<STDIN>)
      {
       print "    <!-- Special case for motor vehicles -->\n";
       print "\n";
-      print "    <if>\n";
-      print "      <output k=\"foot\"       v=\"no\"/>\n";
-      print "      <output k=\"horse\"      v=\"no\"/>\n";
-      print "      <output k=\"wheelchair\" v=\"no\"/>\n";
-      print "      <output k=\"bicycle\"    v=\"no\"/>\n";
-      print "\n";
-      print "      <output k=\"bridge\"        v=\"no\"/>\n";
-      print "      <output k=\"tunnel\"        v=\"no\"/>\n";
-      print "      <output k=\"footroute\"     v=\"no\"/>\n";
-      print "      <output k=\"bicycleroute\"  v=\"no\"/>\n";
-      print "      <output k=\"cyclebothways\" v=\"no\"/>\n";
-      print "    </if>\n";
       print "\n";
      }
 
@@ -49,10 +37,6 @@ while(<STDIN>)
      {
       print "    <!-- Special case for motor vehicles -->\n";
       print "\n";
-      print "    <if>\n";
-      print "      <output k=\"footroute\"    v=\"no\"/>\n";
-      print "      <output k=\"bicycleroute\" v=\"no\"/>\n";
-      print "    </if>\n";
       print "\n";
      }
 
