0146b4b4b4d3187b63e3494ec1b9786f4076a971
Izumi Kawashima <schedul.xor@gmail.com>
2021-10-04 15:16:51 +0900

    Revert walk

--- xml/scripts/drive.pl
+++ xml/scripts/drive.pl
@@ -30,6 +30,18 @@ while(<STDIN>)
      {
       print "    <!-- Special case for motor vehicles -->\n";
       print "\n";
+      print "    <if>\n";
+      print "      <output k=\"foot\"       v=\"no\"/>\n";
+      print "      <output k=\"horse\"      v=\"no\"/>\n";
+      print "      <output k=\"wheelchair\" v=\"no\"/>\n";
+      print "      <output k=\"bicycle\"    v=\"no\"/>\n";
+      print "\n";
+      print "      <output k=\"bridge\"        v=\"no\"/>\n";
+      print "      <output k=\"tunnel\"        v=\"no\"/>\n";
+      print "      <output k=\"footroute\"     v=\"no\"/>\n";
+      print "      <output k=\"bicycleroute\"  v=\"no\"/>\n";
+      print "      <output k=\"cyclebothways\" v=\"no\"/>\n";
+      print "    </if>\n";
       print "\n";
      }
 
@@ -37,6 +49,10 @@ while(<STDIN>)
      {
       print "    <!-- Special case for motor vehicles -->\n";
       print "\n";
+      print "    <if>\n";
+      print "      <output k=\"footroute\"    v=\"no\"/>\n";
+      print "      <output k=\"bicycleroute\" v=\"no\"/>\n";
+      print "    </if>\n";
       print "\n";
      }
 
